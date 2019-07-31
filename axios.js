433cf76c69bddd4f8e4dbfc016b711c28fed6592
Laura-Ella <lauraella.robinson@gmail.com>
2019-07-31 13:54:34 -0400

    updates

--- axios.js
+++ axios.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 
 axios
 .get(
- 'https://sandbox-api.brewerydb.com/v2/breweries/?key=ba145591297f768757f299a230a7773a'
+ 'https://sandbox-api.brewerydb.com/v2/locations/?key=ba145591297f768757f299a230a7773a'
 )
 .then(response => {
  // handle success
  console.log(response.data);
  let stringified = JSON.stringify(response.data);
  console.log(stringified);
- fs.writeFile('./dataBreweries.json', stringified, err => {
+ fs.writeFile('./dataLocations.json', stringified, err => {
    if (err) {
      console.error(err);
    } else {
