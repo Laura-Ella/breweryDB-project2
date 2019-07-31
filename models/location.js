12818098b04e8800ba6eb0ba0f81a61a03f58d5c
Laura-Ella <lauraella.robinson@gmail.com>
2019-07-31 16:11:46 -0400

    updates

--- models/location.js
+++ models/location.js
@@ -2,9 +2,14 @@ const mongoose = require("../db/connection")
 const Schema = mongoose.Schema
 
 const Location = new Schema({ 
-    street: String,
-    city: String,
-    state: String
+    streetAddress: String,
+    locality: String,
+    region: String,
+    postalCode: String,
+    countryIsoCode: String,
+    brewery: {
+        type: String
+    }
 })
 
 module.exports = mongoose.model("Location", Location)
