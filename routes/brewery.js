e3731e086b744dffe29ee5724a0db04bb2004c9f
Laura-Ella <lauraella.robinson@gmail.com>
2019-07-31 18:19:58 -0400

    updates

--- routes/brewery.js
+++ routes/brewery.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const breweryController = require('../controllers/beer')
+const breweryController = require('../controllers/brewery')
 
 router.get('/', breweryController.index)
-router.get('/', breweryController.show)
-router.get('/', breweryController.index)
+router.get('/name/:name', breweryController.show)
 router.post('/', breweryController.create)
-router.put('/', breweryController.edit)
-router.delete('/', breweryController.delete)
+router.put('/:id', breweryController.edit)
+router.delete('/:id', breweryController.delete)
+
+module.exports = router
--- routes/location.js
+++ routes/location.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const locationController = require('../controllers/beer')
+const locationController = require('../controllers/location')
 
 router.get('/', locationController.index)
-router.get('/', locationController.show)
-router.get('/', locationController.index)
+router.get('/name/:name', locationController.show)
 router.post('/', locationController.create)
-router.put('/', locationController.edit)
-router.delete('/', locationController.delete)
+router.put('/:id', locationController.edit)
+router.delete('/:id', locationController.delete)
+
+module.export = router
