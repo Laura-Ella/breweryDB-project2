85bfdd9e80b2f2262a0da6ff9220a92be381e932
Laura-Ella <lauraella.robinson@gmail.com>
2019-07-31 14:03:32 -0400

    updates

--- db/seed.js
+++ db/seed.js
@@ -1,9 +1,13 @@
-const shows = require('./shows.json')
-const people = require('./people.json')
+const dataBeers = require('./dataBeers.json')
+const dataBreweries = require('./dataBreweries.json')
+const dataLocations = require('./dataLocations.json')
+
 console.log(people)
 
 const Person = require('./models/Person')
 const Show = require('./models/Show')
+const Show = require('./models/Show')
+
 
 Person.deleteMany({}).then(Person.create(people))
 Show.deleteMany({}).then(Show.create(shows))
