5f8c5d6ab8bc8ebf99612a63e9f416907aaa802a
Laura-Ella <lauraella.robinson@gmail.com>
2019-07-31 14:14:31 -0400

    udpates

--- controllers/beer.js
+++ controllers/beer.js
@@ -1,40 +1,40 @@
-// const Beer = require("../models/Beer")
+const Beer = require("../models/Beer")
 
-// module.exports = {
-//   index: function (req, res) {
-//     Beer.find({})
-//       .then(beer =>
-//         res.json(beer)
-//       )
-//   },
-//   show: function (req, res) {
-//     Beer.find({ id: req.params.id })
-//       .then(beer =>
-//         res.json(beer)
-//       )
-//   },
-//   showName: function (req, res) {
-//     Beer.find({ name: req.params.name })
-//       .then(beer =>
-//         res.json(beer)
-//       )
-//   },
-//   create: function (req, res) {
-//     Beer.create(req.body)
-//       .then(beer =>
-//         res.json(beer)
-//       )
-//   },
-//   edit: function (req, res) {
-//     Beer.findOneAndUpdate({ _id: req.params.id }, 
-//       {$set: {"name" : req.body.name} } )
-//       .then(beer =>
-//         res.json(beer)
-//       )},
-//   delete: function (req, res) {
-//     Beer.findOneAndDelete({ _id: req.params.id }, req.body)
-//       .then(beer =>
-//         res.json(beer)
-//       )
-//   }
-// }
+module.exports = {
+  index: function (req, res) {
+    Beer.find({})
+      .then(beer =>
+        res.json(beer)
+      )
+  },
+  show: function (req, res) {
+    Beer.find({ id: req.params.id })
+      .then(beer =>
+        res.json(beer)
+      )
+  },
+  showName: function (req, res) {
+    Beer.find({ name: req.params.name })
+      .then(beer =>
+        res.json(beer)
+      )
+  },
+  create: function (req, res) {
+    Beer.create(req.body)
+      .then(beer =>
+        res.json(beer)
+      )
+  },
+  edit: function (req, res) {
+    Beer.findOneAndUpdate({ _id: req.params.id }, 
+      {$set: {"name" : req.body.name} } )
+      .then(beer =>
+        res.json(beer)
+      )},
+  delete: function (req, res) {
+    Beer.findOneAndDelete({ _id: req.params.id }, req.body)
+      .then(beer =>
+        res.json(beer)
+      )
+  }
+}
--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,12 @@
-const beers = require('./dataBeers.json')
-const breweries = require('./dataBreweries.json')
-const locations = require('./dataLocations.json')
+const beers = require('../dataBeers.json')
+const breweries = require('../dataBreweries.json')
+const locations = require('../dataLocations.json')
 
 console.log(locations)
 
-const Beer = require('./models/beer')
-const Brewery = require('./models/brewery')
-const Location = require('./models/location')
+const Beer = require('../models/beer')
+const Brewery = require('../models/brewery')
+const Location = require('../models/location')
 
 
 Beer.deleteMany({}).then(Beer.create(beers))
