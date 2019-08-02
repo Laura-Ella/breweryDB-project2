81e331663abe273c44df35d66099f1b8d81a8881
Laura-Ella <lauraella.robinson@gmail.com>
2019-08-02 09:20:26 -0400

    index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ app.use("/beer", require("./routes/beer"));
 app.use("/brewery", require("./routes/brewery"));
 app.use("/location", require("./routes/location"));
 
-app.listen(4000, () => {
-    console.log("listening on 4000")
-});
-
-// app.set("port", process.env.PORT || 8080);
-
-// app.listen(app.get("port"), () => {
-//   console.log(`✅ PORT: ${app.get("port")} 🌟`);
+// app.listen(4000, () => {
+//     console.log("listening on 4000")
 // });
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
