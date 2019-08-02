9bc30228c63a2d3e65be7000edc980f7ee43c229
Laura-Ella <lauraella.robinson@gmail.com>
2019-08-02 09:47:14 -0400

    updates

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,4 @@
+const mongoose = require('./connection')
 const beers = require('../dataBeers.json')
 const breweries = require('../dataBreweries.json')
 const locations = require('../dataLocations.json')
