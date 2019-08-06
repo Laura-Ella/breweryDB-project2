9b4cc3d01d146b047ebc162059f4bb7f32bf95dc
Laura-Ella <lauraella.robinson@gmail.com>
2019-08-06 15:01:28 -0400

    updates

--- db/seed2.js
+++ db/seed2.js
@@ -4,19 +4,6 @@ const Location = require("../models/location")
 const breweries = require('../dataBreweries.json')
 const locations = require('../dataLocations.json')
 
-// Brewery.find({}).then(allBreweries => {
-//     Location.find({}).then(allLocations => {
-//         for (let i = 0; i < allBreweries.length; i++) {
-//             for (let j = 0; j < allLocations.length; j++) {
-//                 if (allBreweries[i].name === allLocations[j].name) {
-//                     allBreweries[i].location.push(allLocations[j])
-//                     console.log(allBreweries[i].location)
-//                 }
-//             }
-//         }
-//     })
-// })
-
 Location.find({}).then(allLocation => {
     allLocation.forEach( thisLocation => {
         Brewery.findOneAndUpdate({name: thisLocation.name}, {$set: {location:thisLocation._id}},{new:true}).then(updated => {
