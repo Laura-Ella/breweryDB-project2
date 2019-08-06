2bb5b54872c4c91b2b5c1d08988c0b706a6cb3e7
Laura-Ella <lauraella.robinson@gmail.com>
2019-08-06 14:17:44 -0400

    updates

--- db/seed2.js
+++ db/seed2.js
@@ -6,13 +6,11 @@ const locations = require('../dataLocations.json')
 
 Brewery.find({}).then(allBreweries => {
     Location.find({}).then(allLocations => {
-        console.log(allBreweries)
-        console.log(allLocations)
         for (let i = 0; i < allBreweries.length; i++) {
             for (let j = 0; j < allLocations.length; j++) {
-                // console.log(allBreweries)
                 if (allBreweries[i].name === allLocations[j].name) {
                     allBreweries[i].location.push(allLocations[j])
+                    console.log(allBreweries)
                 }
             }
         }
